7332211067dc04c5005fad16c2a13442e744c442
Andri Buehler <andri.buehler@gmail.com>
2020-10-31 14:08:27 +0100

    Add Units to RecipeContext

---
+++ Kooboos.API/Migrations/20201031130750_Add Units to RecipeContext.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kooboos.API.Migrations
+{
+    public partial class AddUnitstoRecipeContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_IngredientsListItems_Unit_UnitId",
+                table: "IngredientsListItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Unit",
+                table: "Unit");
+
+            migrationBuilder.RenameTable(
+                name: "Unit",
+                newName: "Units");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Units",
+                table: "Units",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_IngredientsListItems_Units_UnitId",
+                table: "IngredientsListItems",
+                column: "UnitId",
+                principalTable: "Units",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_IngredientsListItems_Units_UnitId",
+                table: "IngredientsListItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Units",
+                table: "Units");
+
+            migrationBuilder.RenameTable(
+                name: "Units",
+                newName: "Unit");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Unit",
+                table: "Unit",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_IngredientsListItems_Unit_UnitId",
+                table: "IngredientsListItems",
+                column: "UnitId",
+                principalTable: "Unit",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
